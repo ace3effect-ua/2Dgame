1f2f5cedf3ce55b88ac746b685d020776c37aa5c
Asiedu Owusu-Kyereko <aowusuky@email.arizona.edu>
2019-02-28 10:31:43 -0700

    more minor changes

--- Assets/Scripts/soundButton.cs
+++ Assets/Scripts/soundButton.cs
@@ -2,14 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class soundButton : MonoBehaviour
 {
     public AudioMixer mixer;
+    public Image buttonImage;
+
+    public Sprite muteSprite;
+    public Sprite unmuteSprite;
+    bool off;
     // Start is called before the first frame update
     void OnMouseDown(){
         //print( "Clicked the sound button!");
-        mixer.SetFloat("Master", -80.0f);
+        
+        off = !off;
+        if (off == false){
+            mixer.SetFloat("Master", 0.0f);  
+        }
+        else{
+            mixer.SetFloat("Master", -80.0f);
+        }
         //mixer.SetFloat("Music", 0.0f);
 
     }
