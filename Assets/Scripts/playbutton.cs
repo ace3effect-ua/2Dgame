c85b21027460ca6fb12364a5301802d235bb3bcf
Trevor Begay <tbegay1@email.arizona.edu>
2019-02-28 00:42:47 -0700

    Making a play button script

---
+++ Assets/Scripts/playbutton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playbutton : MonoBehaviour
+{
+	public Animator animator;
+
+	public void StartButtonClicked() {
+		animator.SetBool("Exit", true);
+
+		SceneManager.LoadScene( 1 );
+
+
+	}
+}
