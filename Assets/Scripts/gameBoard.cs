ba15c484985c24cf07d5fee7c7c651b62fb67e3d
Asiedu Owusu-Kyereko <aowusuky@email.arizona.edu>
2019-02-28 00:03:44 -0700

    pieces spawning

---
+++ Assets/Scripts/gameBoard.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameBoard : MonoBehaviour
+{
+    public Piece[,] pieces = new Piece[5, 8];
+    public GameObject pennyPrefab;
+    public GameObject dimePrefab;
+
+    void Start(){
+        makeBoard();
+    }
+
+    void makeBoard(){
+        //make pennies
+        for(int y = 0; y < 1; y++){
+            for(int x = 0; x < 4; x++){
+                //generate pennies
+                makePiece(x,y);
+            }
+        }
+    }
+
+    void makePiece(int x, int y){
+        
+        GameObject go = Instantiate(pennyPrefab) as GameObject;
+        go.transform.SetParent(transform);
+        Piece p = go.GetComponent<Piece>();
+        pieces[x,y] = p;
+        //placePiece(p, x, y);
+    }
+
+    //void placePiece(Piece p, int x, int y){
+        //p.transform.position = 
+    //}
+}
---
+++ Assets/Scripts/soundButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class soundButton : MonoBehaviour
+{
+    public AudioMixer mixer;
+    // Start is called before the first frame update
+    void OnMouseDown(){
+        //print( "Clicked the sound button!");
+        mixer.SetFloat("Master", -80.0f);
+        //mixer.SetFloat("Music", 0.0f);
+
+    }
+
+    //void OnMouseUp(){
+        //mixer.SetFloat("Master", 0.0f);
+    //}
+}
